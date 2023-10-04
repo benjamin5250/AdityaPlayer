f2acbe463d3519fa1a2f6f782a134bc6daedbdfd
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-10-04 16:17:16 +0630

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -130,7 +130,7 @@ async def play(_, message: Message):
 
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://te.legra.ph/file/ed6920a2f0ab5af3fd55d.png"
+        thumb_name = "https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -140,7 +140,7 @@ async def play(_, message: Message):
                 [
                         InlineKeyboardButton(
                             text="💥 Jøɩɳ Ɦɘɤɘ & Sʋƥƥøɤʈ 💞",
-                            url=f"https://t.me/adityadiscus")
+                            url=f"https://t.me/purpleplaneteer")
 
                 ]
             ]
@@ -179,7 +179,7 @@ async def play(_, message: Message):
                 [
                         InlineKeyboardButton(
                             text="💥 Jøɩɳ Ɦɘɤɘ & Sʋƥƥøɤʈ 💞",
-                            url=f"https://t.me/adityadiscus")
+                            url=f"https://t.me/purpleplaneteer")
 
                 ]
             ]
@@ -187,7 +187,7 @@ async def play(_, message: Message):
 
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://te.legra.ph/file/ed6920a2f0ab5af3fd55d.png"
+            thumb_name = "https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
@@ -195,7 +195,7 @@ async def play(_, message: Message):
                 [
                         InlineKeyboardButton(
                             text="💥 Jøɩɳ Ɦɘɤɘ & Sʋƥƥøɤʈ 💞",
-                            url=f"https://t.me/adityadiscus")
+                            url=f"https://t.me/purpleplaneteer")
 
                 ]
             ]
@@ -250,7 +250,7 @@ async def play(_, message: Message):
                 [
                         InlineKeyboardButton(
                             text="💥 Jøɩɳ Ɦɘɤɘ & Sʋƥƥøɤʈ 💞",
-                            url=f"https://t.me/adityadiscus")
+                            url=f"https://t.me/purpleplaneteer")
 
                 ]
             ]
@@ -272,7 +272,7 @@ async def play(_, message: Message):
         position = await queues.put(chat_id, file=file_path)
         await message.reply_photo(
             photo="final.png",
-            caption="**💥 Ʌɗɩtyɑ🤞Ʌɗɗɘɗ 💿 Søɳʛ❗️\n🔊 Ʌʈ 💞 Ƥøsɩʈɩøɳ » `{}` 🌷 ...**".format(position),
+            caption="**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕🤞Ʌɗɗɘɗ 💿 Søɳʛ❗️\n🔊 Ʌʈ 💞 Ƥøsɩʈɩøɳ » `{}` 🌷 ...**".format(position),
             reply_markup=keyboard,
         )
     else:
@@ -289,7 +289,7 @@ async def play(_, message: Message):
         await message.reply_photo(
             photo="final.png",
             reply_markup=keyboard,
-            caption="**💥 Ʌɗɩtyɑ🤞Mʋsɩƈ 🎸 Nøω 💞\n🔊 Ƥɭɑyɩɳʛ 😍 ØƤ 🥀 ...**".format(),
+            caption="**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕🤞𝑴𝒖𝒔𝒊𝒄 🎸 𝑵𝒐𝒘 💞\n🔊 Ƥɭɑyɩɳʛ 😍 ØƤ 🥀 ...**".format(),
         )
 
     os.remove("final.png")
@@ -303,8 +303,8 @@ async def play(_, message: Message):
 async def pause(_, message: Message):
     await clientbot.pytgcalls.pause_stream(message.chat.id)
     await message.reply_photo(
-                             photo="https://te.legra.ph/file/f2b5739b266e05c9a2909.png", 
-                             caption="**💥 Ʌɗɩtyɑ 🔈 Mʋsɩƈ🤞Nøω 🥀\n▶️ Ƥɑʋsɘɗ 🌷 ...**"
+                             photo="https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg", 
+                             caption="**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕 🔈 𝑴𝒖𝒔𝒊𝒄🤞𝑵𝒐𝒘 🥀\n▶️ Ƥɑʋsɘɗ 🌷 ...**"
     )
 
 
@@ -314,8 +314,8 @@ async def pause(_, message: Message):
 async def resume(_, message: Message):
     await clientbot.pytgcalls.resume_stream(message.chat.id)
     await message.reply_photo(
-                             photo="https://te.legra.ph/file/391e636040ae189c23cdb.png", 
-                             caption="**💥 Ʌɗɩtyɑ 🔈 Mʋsɩƈ🤞Nøω 🥀\n⏸ Ƥɭɑyɩɳʛ 🌷 ...**"
+                             photo="https://te.legra.ph/file/5e3c7aef33003ba82d358.jpgg", 
+                             caption="**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕 🔈 𝑴𝒖𝒔𝒊𝒄🤞𝑵𝒐𝒘 🥀\n⏸ Ƥɭɑyɩɳʛ 🌷 ...**"
     )
 
 
@@ -330,7 +330,7 @@ async def skip(_, message: Message):
     for x in clientbot.pytgcalls.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("**💥 Ʌɗɩtyɑ 💞 Ɲøʈɦɩɳʛ 🔇\n🚫 Ƥɭɑyɩɳʛ 🌷 ...**")
+        await message.reply_text("**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕 💞 Ɲøʈɦɩɳʛ 🔇\n🚫 Ƥɭɑyɩɳʛ 🌷 ...**")
     else:
         queues.task_done(chat_id)
         
@@ -348,8 +348,8 @@ async def skip(_, message: Message):
 
 
     await message.reply_photo(
-                             photo="https://te.legra.ph/file/4e92cde4f29dbecffb7a7.png", 
-                             caption=f'**💥 Ʌɗɩtyɑ 🔈 Mʋsɩƈ🤞Nøω 🥀\n⏩ Sƙɩƥƥɘɗ 🌷 ...**'
+                             photo="https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg", 
+                             caption=f'**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕 🔈 𝑴𝒖𝒔𝒊𝒄🤞𝑵𝒐𝒘 🥀\n⏩ Sƙɩƥƥɘɗ 🌷 ...**'
    ) 
 
 
@@ -364,8 +364,8 @@ async def stop(_, message: Message):
 
     await clientbot.pytgcalls.leave_group_call(message.chat.id)
     await message.reply_photo(
-                             photo="https://te.legra.ph/file/836a1883cf1dd024f1b7e.png", 
-                             caption="**💥 Ʌɗɩtyɑ 🔈 Mʋsɩƈ🤞Nøω 🥀\n❌ Sʈøƥƥɘɗ 🌷 ...**"
+                             photo="https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg", 
+                             caption="**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕 🔈 𝑴𝒖𝒔𝒊𝒄🤞𝑵𝒐𝒘 🥀\n❌ 𝑺𝒕𝒐𝒑𝒑𝒆𝒅 🌷 ...**"
     )
 
 
@@ -382,6 +382,6 @@ async def admincache(client, message: Message):
     )
 
     await message.reply_photo(
-                              photo="https://te.legra.ph/file/02306701e296bcf8634fa.png",
-                              caption="**💥 Ʌɗɩtyɑ 🔈 Mʋsɩƈ🤞Nøω 🥀\n🔥 Ʀɘɭøɑɗɘɗ 🌷 ...**"
+                              photo="https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg",
+                              caption="**🔮𝑷𝒖𝒓𝒑𝒍𝒆 𝑷𝒍𝒂𝒏𝒆𝒕 🔈 𝑴𝒖𝒔𝒊𝒄🤞𝑵𝒐𝒘 🥀\n🔥 Ʀɘɭøɑɗɘɗ 🌷 ...**"
     )
